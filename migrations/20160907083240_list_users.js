562938127baa0fd4d88336cc356e9da96cd2344f
Maxwell Sands <maxwellsands@Maxwells-MacBook-Air.local>
2016-09-07 08:59:06 -0600

    finished up migrations

---
+++ migrations/20160907083240_list_users.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('list_users', function(table){
+    table.increments();
+    table.integer('list_id').references('lists.id');
+    table.integer('user_id').references('users.id');
+    table.text('user_permission');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('list_users')
+};
